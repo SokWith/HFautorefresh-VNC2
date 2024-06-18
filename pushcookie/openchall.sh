9e93caac857c67825104d7deaf844929179e014a
SokWith <129967025+SokWith@users.noreply.github.com>
2024-06-18 21:56:15 +0800

    Update openchall.sh

--- pushcookie/openchall.sh
+++ pushcookie/openchall.sh
@@ -7,11 +7,12 @@ load_page() {
   # The first argument is the web page url
   url=$1
   # Open the url in a new tab of chromium browser
-  chromium --new-tab $url --window-position=0,0 --window-size=600,600
+  chromium --new-tab $url --window-position=0,0 --window-size=700,500
 
 }
 
-# Load the first web page
-load_page "https://www.bing.com/turing/captcha/challenge"
+# Generate a random UUID using /proc/sys/kernel/random/uuid
+UUID=$(cat /proc/sys/kernel/random/uuid)
 
-
+# Load the first web page with the random UUID
+load_page "https://www.bing.com/turing/captcha/challenge?q=&iframeid=local-gen-$UUID"
